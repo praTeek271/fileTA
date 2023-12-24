a737814e670fd78e3b9c533eb18e1ef09d461a98
prateek271 <prateekpatriotasme@gmail.com>
2023-12-24 20:02:57 +0530

    Add file download functionality

--- fileshare/models.py
+++ fileshare/models.py
@@ -1,10 +1,25 @@
-# models.py
+# fileshare/models.py
 from django.db import models
 from django.contrib.auth.models import User
+from random import randint
 
 class File(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     file = models.FileField(upload_to='uploads/')
-    file_type = models.CharField(max_length=10)  # To store file type (pptx, docx, xlsx)
-    is_encrypted = models.BooleanField(default=False)
-    download_link = models.CharField(max_length=100, blank=True)
+    download_link = models.CharField(max_length=255, blank=True)
+
+    def generate_special_url(self):
+        # You can customize the logic for creating a special URL here
+        username = self.user.username
+        filename = self.file.name
+        numerals = str(randint(100000, 999999))
+        # numerals = "123456789"  # Add more if needed
+
+        # Combine username, filename, and numerals to create a special URL
+        special_url = f"{username}_{numerals}_{filename}"
+
+        # Save the special URL to the download_link field
+        self.download_link = special_url
+        self.save()
+
+        return special_url
--- fileshare/views.py
+++ fileshare/views.py
@@ -2,11 +2,8 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from django.core.signing import Signer
-from django.contrib.auth.models import User
-from rest_framework.decorators import authentication_classes, permission_classes
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from .models import File
 from .serializers import FileSerializer
 
@@ -15,43 +12,36 @@ class FileViewSet(viewsets.ModelViewSet):
     serializer_class = FileSerializer
 
     @action(detail=False, methods=['post'])
-    @authentication_classes([TokenAuthentication])  # Use TokenAuthentication
-    @permission_classes([IsAuthenticated])  # Require authentication
+    @authentication_classes([TokenAuthentication])
+    @permission_classes([IsAuthenticated])
     def upload_file(self, request):
-        # Extract user from the special token obtained during login
-        user = self.extract_user_from_token(request.auth.payload.get('special_token', ''))
-        
-        # Extract file_type from the uploaded file
-        file_type = self.extract_file_type(request.FILES.get('file'))
+        serializer = FileSerializer(data={'file': request.FILES.get('file')})
 
-        # If user or file_type extraction fails, return a 400 response
-        if not user or not file_type:
-            return Response({"message": "Invalid special token or file type"}, status=status.HTTP_400_BAD_REQUEST)
-
-        # Add the extracted information to the request data
-        request.data['file_type'] = file_type
-        request.data['user'] = user
-
-        serializer = FileSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(user=request.user)
+            serializer.save()
             return Response({"message": "File uploaded successfully"}, status=status.HTTP_201_CREATED)
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    @action(detail=True, methods=['get'])
+    def download_file(self, request, pk=None):
+        file_instance = self.get_object()
+
+        if request.user != file_instance.user:
+            return Response({"message": "Access denied"}, status=status.HTTP_403_FORBIDDEN)
+
+        special_url = file_instance.generate_special_url()
+
+        return Response({"download_link": special_url, "message": "success"})
+
     def extract_user_from_token(self, special_token):
-        # You need to implement the logic to extract user from the special token
-        # For example, you can use Django's Signer for simplicity in this example
         try:
+            from django.core.signing import Signer
             signer = Signer()
             user = signer.unsign(special_token)
             return user
         except Exception as e:
-            # Handle the exception (e.g., invalid token)
             return None
 
     def extract_file_type(self, uploaded_file):
-        # Extract file_type from the uploaded file
-        # This is just a basic example, you might need a more robust method
-        if uploaded_file:
-            return uploaded_file.name.split('.')[-1].lower()  # Get the file extension
-        return ''
+        return uploaded_file.name.split('.')[-1].lower() if uploaded_file else ''
