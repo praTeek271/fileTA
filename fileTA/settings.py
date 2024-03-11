8e5b6b5760a382654b54a237123d8a118d56671e
prateek271 <prateekpatriotasme@gmail.com>
2024-03-12 04:42:45 +0530

    Refactor File model and comment out unused code

--- fileTA/settings.py
+++ fileTA/settings.py
@@ -42,10 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
+    # 'rest_framework.authtoken',
     'rest_framework_simplejwt',
     'fileshare',
-    'apiapp',
 ]
 
 MIDDLEWARE = [
--- fileshare/models.py
+++ fileshare/models.py
@@ -3,32 +3,30 @@ from django.db import models
 from django.contrib.auth.models import User as DjangoUser
 from random import randint
 
-class File(models.Model):
-    user = models.ForeignKey(DjangoUser, on_delete=models.CASCADE)
-    file = models.FileField(upload_to='uploads/')
-    download_link = models.CharField(max_length=255, blank=True)
+#//  class File(models.Model):
+    # user = models.ForeignKey(DjangoUser, on_delete=models.CASCADE)
+    # file = models.FileField(upload_to='uploads/')
+    # download_link = models.CharField(max_length=255, blank=True)
 
-    def generate_special_url(self):
-        # You can customize the logic for creating a special URL here
-        username = self.user.username
-        filename = self.file.name
-        numerals = str(randint(100000, 999999))
-        # numerals = "123456789"  # Add more if needed
+    # def generate_special_url(self,filename,username):
+    #     # You can customize the logic for creating a special URL here
+    #     username =  username or self.user.username 
+    #     filename =  filename or self.file.name 
+    #     numerals = str(randint(100000, 999999))
+    #     # numerals = "123456789"  # Add more if needed
 
-        # Combine username, filename, and numerals to create a special URL
-        special_url = f"{username}_{numerals}_{filename}"
-        print("generating url........")
-        # Save the special URL to the download_link field
-        self.download_link = special_url
-        self.save()
+    #     # Combine username, filename, and numerals to create a special URL
+    #     special_url = f"{username}_{numerals}_{filename}"
+    #     print("generating url........")
+    #     # Save the special URL to the download_link field
+    #     self.download_link = special_url
+    #     self.save()
 
-        return special_url
+    #     return special_url
     
 
-    def __str__(self):
-        return self.file.name
-
-
+    # def __str__(self):
+    #     return self.file.name
 
 class User(models.Model):
     username = models.CharField(max_length=50)
