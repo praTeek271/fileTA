002421ce35a9d12e3d49ad56f4d99cc8d6adab9e
prateek271 <prateekpatriotasme@gmail.com>
2024-03-12 02:50:06 +0530

    Update DATABASES settings in settings.py

--- fileTA/settings.py
+++ fileTA/settings.py
@@ -85,8 +85,10 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-    # "default": dj_database_url.parse(os.environ.get(DATABASE_URL))
 }
+
+# DATABASES['default'] = dj_database_url.parse(os.environ.get(DATABASE_URL))
+DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
 
 
 # Password validation
